e20d0f4a3041db3d6afc84f1c3004c0993f9ca19
toletole412 <yoonjioh90@gmail.com>
2018-03-11 22:17:44 +0100

    Add initialsatet for board

---
+++ games/router.js
@@ -0,0 +1,44 @@
+const Router = require("express").Router;
+const Games = require('./model')
+const router = new Router();
+const { fillBoard, percentageFilled, numberOfValues, cols, rows, isPossibleMove,
+  removeRandomValuesFromBoard, filledPositions } = require("./gameLogic");
+
+
+
+router.post("/games", (req, res) => {
+  const initialState = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+  ]
+
+  const game = {
+    board: JSON.stringfy(initialState),
+    locked: locked,
+    sidebar: sidebar
+  }
+
+  Games
+    .create(game)
+    .then(entity => {
+      res.status(201);
+      res.send({
+        board: entity.board,
+        locked: entity.locked //this is not Sure
+        sidebar: entity.sidebar
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send({
+        message: "Something went wrong"
+      });
+    })
+  };
+
+
+module.exports = router;
